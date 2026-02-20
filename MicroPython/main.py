064d42e48d908f64632a404b57530ff441d6b1d2
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-20 21:40:20 +0000

    Created the program in python and hexified it

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,25 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Jet Lu
+Created on: Feb 2026
+This module is a Micro:bit MicroPython program that will tell you the temperature.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+temperature = 0
+import time
+
+# reset
+display.clear
+display.show(Image.HAPPY)
+time.sleep(1000)
+
+# temperature will display when A is pressed
+while True:
+    if button_a.is_pressed():
+        temperature = input.temperature()
+        print("The temperature is")
+        print(temperature)
+        time.sleep(1000)
+        display.clear  # reset
+        display.show(Image.HAPPY)
