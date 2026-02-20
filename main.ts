8359b75be02d7b3be7c0861b1ec5dbcd15bd6255
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-20 13:21:32 -0500

    Created the program to dispaly temp

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Feb 2026
+ * This program will display the temperature.
 */
+let temperature: number
 
-basic.showString('Hello, World!')
+//reset
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Temperature will display when A is pressed
+input.onButtonPressed(Button.A, function () {
+  temperature = input.temperature()
+  basic.showString("The temperature is")
+  basic.showNumber(temperature)
+  pause(1000)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
