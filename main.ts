d87f3da91ee58ca577bee58e0683c31cce3fcce1
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-04-07 12:54:29 -0400

    fix

--- main.ts
+++ main.ts
@@ -4,15 +4,17 @@
  * Created on: Feb 2026
  * This program will display the temperature.
 */
+
 let temperature: number
 
-//reset
+// reset
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// Temperature will display when A is pressed
+// temperature will display when button a is pressed
 input.onButtonPressed(Button.A, function () {
   temperature = input.temperature()
+  basic.clearScreen()
   basic.showString("The temperature is")
   basic.showNumber(temperature)
   pause(1000)
