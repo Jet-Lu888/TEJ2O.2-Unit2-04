7844844f1826762cf83ca526718798b0ea7bfb92
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-04-08 17:07:38 +0000

    update

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,20 +6,19 @@ This module is a Micro:bit MicroPython program that will tell you the temperatur
 
 from microbit import *
 
-temperature = 0
-import time
+numTemp = 0
 
 # reset
 display.clear
 display.show(Image.HAPPY)
-time.sleep(1000)
+sleep(1000)
 
 # temperature will display when A is pressed
 while True:
-    if button_a.is_pressed():
-        temperature = input.temperature()
-        print("The temperature is")
-        print(temperature)
-        time.sleep(1000)
+    if button_a.was_pressed():
+        numTemp = temperature()
+        display.scroll("The temperature is")
+        display.scroll(numTemp)
+        sleep(1000)
         display.clear  # reset
         display.show(Image.HAPPY)
